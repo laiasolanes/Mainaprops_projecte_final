fd4ef81980950eb21fd8e336b9f7df7705c48fde
Laia Solanes <laiasolanes@gmail.com>
2021-03-06 09:52:44 +0100

    Confirg firebase

--- mainaprops/src/App.jsx
+++ mainaprops/src/App.jsx
@@ -5,6 +5,7 @@ function App() {
   return (
     <div className="App">
       <h1>Hola</h1>
+      <img src="https://firebasestorage.googleapis.com/v0/b/mainaprops.appspot.com/o/avatar_bici.png?alt=media&token=515a2986-d2f0-427e-a83e-5ca8f1d1e817" alt="bicicleta" />
     </div>
   );
 }
---
+++ mainaprops/src/firebase/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: 'AIzaSyCiDrvfPMo7GP3haWaA5M7c2vUeTUyUlCY',
+  authDomain: 'mainaprops.firebaseapp.com',
+  projectId: 'mainaprops',
+  storageBucket: 'mainaprops.appspot.com',
+  messagingSenderId: '602132883769',
+  appId: '1:602132883769:web:d2ac1f09ee0ad424c360e8',
+};
+
+export default firebaseConfig;
