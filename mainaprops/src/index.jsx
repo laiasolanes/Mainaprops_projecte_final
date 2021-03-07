5c049e9b5eabd1d7be3e7a30c6c2ad5e62c49bd2
Laia Solanes <laiasolanes@gmail.com>
2021-03-07 12:50:45 +0100

    Install material-ui and create a modal view example

--- mainaprops/src/index.jsx
+++ mainaprops/src/index.jsx
@@ -2,10 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import ModalView from './components/modalView';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <ModalView />
   </React.StrictMode>,
   document.getElementById('root'),
 );
