f4d1bd351b3739833c59cc1dfbcda34a71d2d163
Laia Solanes <laiasolanes@gmail.com>
2021-03-10 11:46:39 +0100

    Create action insert new user in usersList component

--- mainaprops/src/components/usersList/usersList.jsx
+++ mainaprops/src/components/usersList/usersList.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable no-underscore-dangle */
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import './usersList.css';
 import { connect } from 'react-redux';
 import { PropTypes } from 'prop-types';
@@ -7,12 +7,103 @@ import { bindActionCreators } from 'redux';
 import DeleteRoundedIcon from '@material-ui/icons/DeleteRounded';
 import EditRoundedIcon from '@material-ui/icons/EditRounded';
 import Button from '@material-ui/core/Button';
-import loadUsers from '../../redux/actions/actionCreators';
+import { makeStyles } from '@material-ui/core/styles';
+import { Modal, TextField } from '@material-ui/core';
+import { loadUsers, insertUser } from '../../redux/actions/actionCreators';
 
+const useStyles = makeStyles((theme) => ({
+  modal: {
+    position: 'absolute',
+    width: '98%',
+    backgroundColor: '#6CC3C6',
+    border: '3px solid #ffffff',
+    color: '#ffffff',
+    borderRadius: '10px',
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(8, 4, 8, 4),
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+  },
+  textfield: {
+    width: '100%',
+  },
+  inputMaterial: {
+    width: '100%',
+  },
+  button_violet: {
+    width: '100%',
+    backgroundColor: '#3D2563',
+    color: '#ffffff',
+    textTransform: 'none',
+    margin: '10px 0',
+    padding: '10px 0',
+    borderRadius: '50px',
+    boxShadow: theme.shadows[2],
+  },
+  button_outlined: {
+    width: '100%',
+    backgroundColor: '#6CC3C6',
+    color: '#ffffff',
+    textTransform: 'none',
+    margin: '10px 0',
+    padding: '8px 0',
+    borderRadius: '50px',
+    border: '3px solid #ffffff',
+    boxShadow: theme.shadows[2],
+  },
+}));
 function UsersList({ users, actions }) {
+  const styles = useStyles();
+
+  const [modalInsert, setModalInsert] = useState(false);
+
+  const [userSelected, setUserSelected] = useState({
+    name: '',
+    age: '',
+    image: '',
+  });
+
+  const openCloseModalInsert = () => {
+    setModalInsert(!modalInsert);
+  };
+
+  const handelChange = (event) => {
+    const { name, value } = event.target;
+    setUserSelected((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
+
   useEffect(() => {
     actions.loadUsers();
-  }, []);
+  }, [users]);
+
+  function clickAddUser() {
+    actions.insertUser(userSelected);
+    openCloseModalInsert();
+  }
+
+  const bodyInsertar = (
+    <div className={styles.modal}>
+      <h2>Alta usuari</h2>
+      <TextField name="name" className={styles.inputMaterial} label="Nom" onChange={handelChange} />
+      <TextField name="age" className={styles.inputMaterial} label="Edat" onChange={handelChange} />
+      <TextField name="image" className={styles.inputMaterial} label="Avatar" onChange={handelChange} />
+
+      <Button
+        className={styles.button_violet}
+        onClick={() => clickAddUser()}
+      >
+        Afegir
+
+      </Button>
+      <br />
+      <Button className={styles.button_outlined} onClick={openCloseModalInsert}>Cancelar</Button>
+
+    </div>
+  );
 
   return (
     <section className="users-list">
@@ -31,7 +122,14 @@ function UsersList({ users, actions }) {
           ))
       }
 
-      <Button variant="contained" className="button--outlined">+ usuaris</Button>
+      <Button variant="contained" className="button--outlined" onClick={openCloseModalInsert}>+ usuaris</Button>
+
+      <Modal
+        open={modalInsert}
+        onClose={openCloseModalInsert}
+      >
+        {bodyInsertar}
+      </Modal>
 
     </section>
   );
@@ -41,6 +139,7 @@ UsersList.propTypes = {
   users: PropTypes.shape([]).isRequired,
   actions: PropTypes.shape({
     loadUsers: PropTypes.func,
+    insertUser: PropTypes.func,
   }).isRequired,
 };
 function mapStateToProps(state) {
@@ -48,6 +147,6 @@ function mapStateToProps(state) {
 }
 
 function mapDispatchToProps(dispatch) {
-  return { actions: bindActionCreators({ loadUsers }, dispatch) };
+  return { actions: bindActionCreators({ loadUsers, insertUser }, dispatch) };
 }
 export default connect(mapStateToProps, mapDispatchToProps)(UsersList);
--- mainaprops/src/redux/actions/actionCreators.js
+++ mainaprops/src/redux/actions/actionCreators.js
@@ -3,7 +3,7 @@ import actionTypes from './actionTypes';
 
 const url = 'http://localhost:5000/api/users';
 
-export default function loadUsers() {
+export function loadUsers() {
   return async function dispatchUsersList(dispatch) {
     const response = await axios.get(url);
     const dataUsers = response.data;
@@ -11,6 +11,17 @@ export default function loadUsers() {
     dispatch({
       type: actionTypes.LOAD_USERS,
       dataUsers,
+    });
+  };
+}
+
+export function insertUser(newUser) {
+  return async function dispatchNewUser(dispatch) {
+    const newUserData = await axios.post(url, { user_profile: newUser });
+
+    dispatch({
+      type: actionTypes.UPDATE_USER,
+      newUserData,
     });
   };
 }
