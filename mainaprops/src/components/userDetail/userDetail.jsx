a01efc26e0d4c845f5f5afdd04e0134301d085f9
Laia Solanes <laiasolanes@gmail.com>
2021-03-12 16:50:23 +0100

    User detail component use object static data

--- mainaprops/src/components/userDetail/userDetail.jsx
+++ mainaprops/src/components/userDetail/userDetail.jsx
@@ -1,8 +1,69 @@
-import { Button } from '@material-ui/core';
-import React from 'react';
+import React, { useState } from 'react';
+import { Modal, Button } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
 import './userDetail.css';
+import userData from '../../constants/usersData';
+
+// console.log(userData);
+
+const pageURL = window.location.href;
+const idUser = pageURL.substr(pageURL.lastIndexOf('/') + 1);
+
+const userDetail = userData.filter((element) => element._id === idUser);
+console.log(userDetail[0]?.user_profile.challenges);
+
+const useStyles = makeStyles((theme) => ({
+  modal: {
+    position: 'absolute',
+    width: '98%',
+    backgroundColor: '#6CC3C6',
+    border: '3px solid #ffffff',
+    color: '#ffffff',
+    borderRadius: '10px',
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(8, 4, 8, 4),
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+  },
+
+  button_violet: {
+    width: '100%',
+    backgroundColor: '#3D2563',
+    color: '#ffffff',
+    fontWeight: '600',
+    textTransform: 'none',
+    margin: '10px 0',
+    padding: '10px 0',
+    borderRadius: '50px',
+    boxShadow: theme.shadows[2],
+    '&:hover': {
+      backgroundColor: '#4d2d80',
+    },
+  },
+
+}));
 
 export default function UserDetailComponent() {
+  const styles = useStyles();
+
+  const [modalChallenge, setModalChallenge] = useState(false);
+
+  function openCloseModalChallenge() {
+    setModalChallenge(!modalChallenge);
+  }
+
+  const body = (
+    <div className={styles.modal}>
+      <h3>Espectacle en família</h3>
+      <div>
+        <Button className={styles.button_violet} onClick={openCloseModalChallenge}>Guardar</Button>
+        <Button className={styles.button_violet} onClick={openCloseModalChallenge}>Cancelar</Button>
+      </div>
+
+    </div>
+  );
+
   return (
     <section className="user__detail">
       <article className="user__header">
@@ -10,7 +71,7 @@ export default function UserDetailComponent() {
         <h3>
           Hola
           <br />
-          Sança!
+          {userDetail[0]?.user_profile.name}
         </h3>
 
         <p>Estàs apunt d’aconseguir els teus propòsits</p>
@@ -56,11 +117,18 @@ export default function UserDetailComponent() {
         <Button
           variant="contained"
           className="button--turquoise"
+          onClick={openCloseModalChallenge}
         >
           Veure repte
         </Button>
       </article>
 
+      <Modal
+        open={modalChallenge}
+        onClose={openCloseModalChallenge}
+      >
+        {body}
+      </Modal>
     </section>
   );
 }
--- mainaprops/src/constants/usersData.js
+++ mainaprops/src/constants/usersData.js
@@ -57,6 +57,64 @@ const usersData = [
       ],
     },
   },
+  {
+    _id: '604a30960b42dfbf0e4ff291',
+    user_profile: {
+      name: 'Javi',
+      age: '9',
+      image: 'https://firebasestorage.googleapis.com/v0/b/mainaprops.appspot.com/o/avatar_bici.png?alt=media&token=d7a1b930-c413-49b5-b43c-004811d800b5',
+      challenges: [
+        {
+          user_id: '6049f9ef0b42dfbf0e2ca4e0',
+          end_date: null,
+          completed: false,
+          reward: {
+            _id: '604b2d9bac0550dffc0ce87e',
+            name: 'Escapada familiar',
+            image: 'https://firebasestorage.googleapis.com/v0/b/mainaprops.appspot.com/o/reward_escapada.png?alt=media&token=3a5ab181-d941-48e2-99a2-d8537e83c3e2',
+          },
+          tasks: [
+            {
+              _id: '604b32cbac0550dffc0ce884',
+              name: 'Cuidar la mascota',
+              image: 'https://firebasestorage.googleapis.com/v0/b/mainaprops.appspot.com/o/task_mascota.png?alt=media&token=849935de-06d3-401f-89c1-dd555f5dd9b3',
+              times: 7,
+            },
+            {
+              _id: '604b3388ac0550dffc0ce885',
+              name: 'Practicar instrument',
+              image: 'https://firebasestorage.googleapis.com/v0/b/mainaprops.appspot.com/o/task_instrument.png?alt=media&token=45803749-6a9e-4b5a-9800-32742a7a06ff',
+              times: 5,
+            },
+          ],
+        },
+        {
+          user_id: '6049f9ef0b42dfbf0e2ca4e0',
+          end_date: null,
+          completed: false,
+          reward: {
+            _id: '604b3251ac0550dffc0ce883',
+            name: 'Nit amb amics',
+            image: 'https://firebasestorage.googleapis.com/v0/b/mainaprops.appspot.com/o/reward_nit_amics.png?alt=media&token=23f34930-3f4b-40e8-9524-3c3458045990',
+          },
+          tasks: [
+            {
+              _id: '604b3470ac0550dffc0ce888',
+              name: 'Parar taula',
+              image: 'https://firebasestorage.googleapis.com/v0/b/mainaprops.appspot.com/o/task_parar.png?alt=media&token=aeede4ac-3f4c-411f-bc62-c62dbb54b333',
+              times: 7,
+            },
+            {
+              _id: '604b34d9ac0550dffc0ce889',
+              name: 'Fer els deures',
+              image: 'https://firebasestorage.googleapis.com/v0/b/mainaprops.appspot.com/o/task_deures.png?alt=media&token=37e5e0cd-9c7c-449f-bcf5-fcc6c24b2d40',
+              times: 5,
+            },
+          ],
+        },
+      ],
+    },
+  },
 
 ];
 
