1f76265665704c9bcfaa97be0bff5ca4f191bac5
Laia Solanes <laiasolanes@gmail.com>
2021-03-21 17:48:49 +0100

    Create adminUsersComponent

--- mainaprops/src/components/logIn/logIn.jsx
+++ mainaprops/src/components/logIn/logIn.jsx
@@ -1,13 +1,8 @@
 import React from 'react';
-import { connect } from 'react-redux';
-import { PropTypes } from 'prop-types';
-import { bindActionCreators } from 'redux';
 import './logIn.css';
-import '../../firebase';
 import Button from '@material-ui/core/Button';
-import { loginWithGoogle } from '../../redux/actions/actionCreators';
 
-function LogIn({ admin, actions }) {
+export default function LogIn() {
   return (
     <section className="login">
 
@@ -19,21 +14,10 @@ function LogIn({ admin, actions }) {
         variant="contained"
         className="button--violet-big"
         href="/"
-        onClick={actions.loginWithGoogle}
       >
         Accedir amb google
-
       </Button>
-      {
-         admin.isLogged && (
-         <>
-           <h2>Hola!</h2>
-           <p>{admin.displayName}</p>
-           <p>{admin.email}</p>
-           <img src={admin.photoURL} alt={admin.displayName} />
-         </>
-         )
-}
+
       <p>
         Una aplicació per motivar els teus fills a complir els seus propòsits.
         Crea un repte, assigna tasques i defineix la recompensa.
@@ -44,24 +28,3 @@ function LogIn({ admin, actions }) {
     </section>
   );
 }
-LogIn.propTypes = {
-  admin: PropTypes.shape({
-    isLogged: PropTypes.bool,
-    displayName: PropTypes.string,
-    email: PropTypes.string,
-    photoURL: PropTypes.string,
-  }).isRequired,
-  actions: PropTypes.shape({
-    loginWithGoogle: PropTypes.func,
-  }).isRequired,
-};
-
-function mapStateToProps(state) {
-  return { admin: state.admin };
-}
-
-function mapDispatchToProps(dispatch) {
-  return { actions: bindActionCreators({ loginWithGoogle }, dispatch) };
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(LogIn);
