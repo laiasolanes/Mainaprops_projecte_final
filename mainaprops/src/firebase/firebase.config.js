7695cd3ef85b706fd5e582f1be1251b849770318
Laia Solanes <laiasolanes@gmail.com>
2021-03-23 10:01:41 +0100

    Firebase config in .env file

--- mainaprops/src/firebase/firebase.config.js
+++ mainaprops/src/firebase/firebase.config.js
@@ -1,10 +1,10 @@
 const firebase = {
-  apiKey: 'AIzaSyCiDrvfPMo7GP3haWaA5M7c2vUeTUyUlCY',
-  authDomain: 'mainaprops.firebaseapp.com',
-  projectId: 'mainaprops',
-  storageBucket: 'mainaprops.appspot.com',
-  messagingSenderId: '602132883769',
-  appId: '1:602132883769:web:d2ac1f09ee0ad424c360e8',
+  apiKey: process.env.APIKEY,
+  authDomain: process.env.AUTHDOMAIN,
+  projectId: process.env.PROJECTID,
+  storageBucket: process.env.STORAGEBUCKET,
+  messagingSenderId: process.env.MESSAGINSENDERID,
+  appId: process.env.APPID,
 };
 
 export default firebase;
